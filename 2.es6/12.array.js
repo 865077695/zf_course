04c5dc2bc54118277614853f9fbe868410277c91
zhiq <865077695@qq.com>
2019-12-27 11:18:42 +0800

    复习redux中的新旧compose方法

--- 2.es6/12.array.js
+++ 2.es6/12.array.js
@@ -31,16 +31,20 @@ console.log(addCurrent(len(sum('abc', 'bed'))))
 
 console.log('compose', compose(addCurrent, len, sum)('abc', 'bed'));
 // 老redux
-// function compose(...args) {
-//     return function (...val) {
-//         let lastFn = args.pop()(...val);    // 先取出最后一个函数,作为reduceRight的第二个参数
-//         return args.reduceRight((prev, current) => {
-//             return current(prev);   // 上个函数的返回值作为这个函数的参数
-//         }, lastFn);
+// function compose(...args) {  // compose接收一个要执行的函数列表并返回一个函数
+//     return function (...val) {    // compose返回的函数接收一些参数并需要将执行结果传递给函数参数列表中的上一个
+//         let fn = args.pop();    // compose的函数列表参数倒序执行，先将最后一个取出来并传入val参数
+//         args.reduceRight((prev, next) => {
+//             return next(prev);   // 上个函数的返回值作为这个函数的参数
+//         }, fn(...val))
 //     }
 // }
-// 新redux
+
+// const compose = (...args) => (...val) => { args.reduceRight((prev, next) => next(prev), args.pop()(...val)) }
+
+// 新redux  先组合三个函数后在返回最终函数去执行参数
 function compose(...args) {
+    // 先遍历函数列表，返回一个函数，用返回的函数去处理后面传入的参数。这样前面遍历函数的结果应该是几个函数一个套一个:addcurrency(len(sum(...val)))
     return args.reduce((prev, current) => {
         return function (...val) {
             return prev(current(...val))
